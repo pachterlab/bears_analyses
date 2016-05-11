86db467d7f6a05a829ee08af816115633a8a7745
Pascal Sturmfels <psturm@umich.edu>
2016-05-11 14:33:59 -0700

    fixed up sleuth.R to newest syntax

--- sleuth.R
+++ sleuth.R
@@ -1,6 +1,9 @@
 #Usage: Rscript sleuth.R <base_directory> <study_design_file> <conditionbase>
 #assumes study_design_file is in base_directory
 
+library(rjsonlite)
+design_file <- readJSON(args[2])
+
 #Remove the line below before shipping
 .libPaths(c(.libPaths(), '/home/psturm/R/x86_64-pc-linux-gnu-library/3.2'))
 args = commandArgs(trailingOnly=TRUE)
@@ -14,8 +17,11 @@ s2c <- read.table(file.path(args[2]), header = TRUE, stringsAsFactors=FALSE)
 s2c <- dplyr::select(s2c, sample = run, condition)
 s2c <- dplyr::mutate(s2c, path = kal_dirs) 
 
-so <- sleuth_prep(s2c, ~condition)
+so <- sleuth_prep(s2c, as.formula(design_file$full_model))
 so <- sleuth_fit(so)
-so <- sleuth_wt(so, which_beta = paste0('condition', args[3])
+so <- sleuth_fit(so, "reduced", as.formula(design_file$reduced_model))
 
-sleuth_live(so)
+so <- sleuth_lrt(so, "reduced:full") 
+
+sleuth_deploy(so)
+
