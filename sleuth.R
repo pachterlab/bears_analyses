270f768e12157e57a9f56331b689dd7198833add
Pascal Sturmfels <psturm@umich.edu>
2016-05-25 11:39:54 -0700

    Handles tsv files correctly

--- sleuth.R
+++ sleuth.R
@@ -12,7 +12,7 @@ base_dir <- args[1]
 sample_id <- dir(file.path(base_dir, "results"))
 kal_dirs <- sapply(sample_id, function(id) file.path(base_dir, "results", id, "kallisto"))
 kal_dirs <- rev(kal_dirs)
-s2c <- read.table(file.path(args[2]), header = TRUE, stringsAsFactors=FALSE)
+s2c <- read.table(file.path(args[2]), header = TRUE, stringsAsFactors=FALSE, sep="\t")
 s2c <- dplyr::select(s2c, sample = run, condition)
 s2c <- dplyr::mutate(s2c, path = kal_dirs) 
 
